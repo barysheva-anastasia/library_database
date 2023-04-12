de87a4303777bc542f02c9eb53e61a62a70cae56
barysheva-anastasia <83612960+barysheva-anastasia@users.noreply.github.com>
2023-04-12 14:29:51 +0300

    Update and rename состояние_книги.sql to select_book's_status.sql

--- select_book's_status.sql
+++ select_book's_status.sql
@@ -1,4 +1,3 @@
---- Для книги "КЭД - странная теория света и вещества" показать все ее состояния за 5 последних лет.
 CREATE OR ALTER VIEW book_states
 AS (
 SELECT 
@@ -12,7 +11,7 @@ FROM READER
 	JOIN BOOK ON DEAL.book_id = BOOK.book_id 
 	JOIN EDITION ON BOOK.edition_id = EDITION.edition_id 
 	JOIN BOOK_TITLE ON EDITION.book_title_id = BOOK_TITLE.book_title_id
-WHERE (BOOK_TITLE.name = 'КЭД - странная теория света и вещества' 
+WHERE (BOOK_TITLE.name = 'ГЉГќГ„ - Г±ГІГ°Г Г­Г­Г Гї ГІГҐГ®Г°ГЁГї Г±ГўГҐГІГ  ГЁ ГўГҐГ№ГҐГ±ГІГўГ ' 
 	AND (DATEDIFF(year, DEAL.lending_date, GETDATE()) < 5 OR DATEDIFF(year, DEAL.returning_date, GETDATE()) < 5))
 )
 GO
